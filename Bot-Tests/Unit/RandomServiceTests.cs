ca662120496599289e92390abcdc7ceaa7a16d1a
Hopson JR <2005998@cognizant.com>
2021-01-20 00:04:42 -0600

    Addresses code smell of unused property

--- Bot-Tests/Unit/RandomServiceTests.cs
+++ Bot-Tests/Unit/RandomServiceTests.cs
@@ -6,8 +6,7 @@ namespace Bot.Tests.Unit
 {
     public class RandomServiceTests
     {
-        private readonly IRandomService randomService;
-        private readonly Random random;
+        private readonly IRandomService randomService;        private readonly Random random;
 
         public RandomServiceTests()
         {
